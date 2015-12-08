c74bd1ee5e1c62c1dec0d3f69d044c4ce374d40b
paulogualter <paulogualter@ig.com.br>
2015-12-08 11:29:09 -0300

    Encurta Link API migre.me

---
+++ EncurtarLink/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace EncurtarLink
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtMigreMe = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL Original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "URL encurtada com migre.me";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(16, 30);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(421, 20);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // txtMigreMe
+            // 
+            this.txtMigreMe.Location = new System.Drawing.Point(16, 90);
+            this.txtMigreMe.Name = "txtMigreMe";
+            this.txtMigreMe.Size = new System.Drawing.Size(421, 20);
+            this.txtMigreMe.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encurtar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 189);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMigreMe);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtMigreMe;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EncurtarLink/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncurtarLink
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+
+            {
+
+                DataSet ds = new DataSet();
+
+                string apiMigreMe = "http://migre.me/api.xml?url=" + txtUrl.Text;
+
+                Application.DoEvents();
+
+                ds.ReadXml(apiMigreMe);
+
+                //O retorno vem em formato XML
+                //<? xml version = "1.0" encoding = "ISO-8859-1" ?>
+                //    < item >
+                //    < error > 0 </ error >
+                //    < errormessage > OK </ errormessage >
+                //< created_at > 2011 - 04 - 01 20:26:44 </ created_at >
+                //    < source > site </ source >
+                //    < date > 2011 - 04 - 01 20:26:44 </ date >
+                //    < id > 4ahU7 </ id >
+                //    < url > http://www.google.com.br/search?hl=pt-BR&q=infopod</url>
+                //< migre > http://migre.me/4ahU7</migre>
+                //< category > free </ category >
+                //    < time > 0.00839495658875 </ time >
+                //    </ item >
+
+                txtMigreMe.Text = ds.Tables[0].Rows[0]["migre"].ToString();
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                throw ex;
+
+            }
+
+        }
+    }
+}
